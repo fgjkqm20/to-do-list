f35944a4fe83b4c8cf27ec79c956754f1cd9dad2
Koyubin <yubindev1998@gmail.com>
2024-03-27 09:48:32 +0900

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import { Container } from '@mui/material';
-import TodoHead from './components/TodoHead/TodoHead.jsx';
-import TodoList from './components/TodoList/TodoList.jsx';
-import TodoCreate from './components/TodoCreate/TodoCreate.jsx';
+import Container from '@mui/material/Container';
+import TodoHead from './components/todo-head/TodoHead.js';
+import TodoListWrapper from './components/todo-list/TodoListWrapper.js';
+import TodoForm from './components/todo-form/TodoForm.js';
 
 /**
- * 애플리케이션의 메인 컴포넌트
- * @returns {JSX.Element} 애플리케이션 컴포넌트
+ * Application Main Component
+ * @returns {JSX.Element} Application Component
  */
 function App() {
   return (
     <>
-      {/* 최대 너비가 'small'인 컨테이너 */}
       <Container maxWidth='sm'>
-        {/* 할 일 목록의 상단 부분 */}
         <TodoHead />
-        {/* 할 일 목록 */}
-        <TodoList />
-        {/* 새로운 할 일 생성 양식 */}
-        <TodoCreate />
+        <TodoListWrapper />
+        <TodoForm />
       </Container>
     </>
   );
