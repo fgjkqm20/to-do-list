6b98063ff4d27e1e4b1b843b127f0a09e655839a
Koyubin <yubindev1998@gmail.com>
2024-03-27 09:48:42 +0900

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@mui/material/CssBaseline';
-import App from './App';
+import App from './App.js';
 
-// DOM에서 'root' 엘리먼트를 찾아 React의 루트를 생성
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// 루트에 앱을 렌더링
 root.render(
   <React.StrictMode>
-    {/* 기본 CSS를 재설정하는 MUI의 CssBaseline */}
     <CssBaseline />
-    {/* 애플리케이션의 메인 컴포넌트 */}
     <App />
   </React.StrictMode>
 );
